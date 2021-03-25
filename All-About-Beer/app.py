192315199b62c59bb3be7be6667d1a0880a33a43
Sooyeon  Kim <71349348+SaraKim-sy@users.noreply.github.com>
2021-03-24 21:20:24 -0400

    fixed

--- All-About-Beer/app.py
+++ All-About-Beer/app.py
@@ -142,32 +142,6 @@ def home():
 
 @app.route('/recommend_a', methods=['GET', 'POST'])
 def recommend_a():
-    _js_escapes = {
-        '\\': '\\u005C',
-        '\'': '\\u0027',
-        '"': '\\u0022',
-        '>': '\\u003E',
-        '<': '\\u003C',
-        '&': '\\u0026',
-        '=': '\\u003D',
-        '-': '\\u002D',
-        ';': '\\u003B',
-        u'\u2028': '\\u2028',
-        u'\u2029': '\\u2029'
-    }
-    # Escape every ASCII character with a value less than 32.
-    _js_escapes.update(('%c' % z, '\\u%04X' % z) for z in xrange(32))
-    def jinja2_escapejs_filter(value):
-            retval = []
-            for letter in value:
-                    if _js_escapes.has_key(letter):
-                            retval.append(_js_escapes[letter])
-                    else:
-                            retval.append(letter)
-
-            return jinja2.Markup("".join(retval))
-    JINJA_ENVIRONMENT.filters['escapejs'] = jinja2_escapejs_filter
-
     if request.method == 'POST':
         # Receives input from the front from POST
         beer_name = request.form["beerName"]
